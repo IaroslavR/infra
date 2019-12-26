242bd6dba3bb20a343ab1736d49f86a59c92d71f
IaroslavR <iarruss@ya.ru>
2019-12-26 18:46:51 +0000

    [add] terragrunt update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+I_PATH=/home/iaro/opt/
+TERRAGRUNT=https://github.com/gruntwork-io/terragrunt/releases/download/v0.21.10/terragrunt_linux_amd64
+TERRAFORM=https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip
+
+
 # Terraform
-wget -O tmp.zip https://releases.hashicorp.com/terraform/0.12.18/terraform_0.12.18_linux_amd64.zip && \
-  unzip -o tmp.zip -d /home/iaro/opt/ && \
+wget -O tmp.zip $TERRAFORM && \
+  unzip -o tmp.zip -d $I_PATH && \
   rm tmp.zip  && \
-  sudo ln -sf /home/iaro/opt/terraform /usr/bin/terraform  && \
+  sudo ln -sf ${I_PATH}terraform /usr/bin/terraform  && \
   terraform -version
+
+# Terragrunt
+wget -O ${I_PATH}terragrunt $TERRAGRUNT && \
+  sudo ln -sf ${I_PATH}terragrunt /usr/bin/terragrunt  && \
+  terragrunt -version
